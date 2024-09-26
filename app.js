de487b445b811a63d1b73171c718b390e04dffa2
Hasoo786 <hk51413121@gmail.com>
2024-09-27 04:57:56 +0500

    time-date

---
+++ app.js
@@ -0,0 +1,43 @@
+
+
+// var rightNow = new Date()
+
+// var days = ["sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thurday" , "Friday" , "Saturday"]
+
+// console.log(rightNow.toString());
+
+// console.log(days[rightNow.getDay()]);
+
+// console.log(rightNow.getFullYear());
+
+// console.log(rightNow.getMonth());
+
+// console.log(rightNow.getSeconds());
+
+// console.log(rightNow.getMilliseconds());
+
+// console.log(rightNow.getTime() / 1000 / 60 / 60 / 24 );
+
+
+
+
+
+
+// function age (dateOfBirth) {
+
+//     var myAge = new Date (dateOfBirth).getTime()
+
+//     var rightNow = new Date ().getTime() 
+
+//     var reMain = myAge - rightNow 
+
+//     console.log (Math.floor(reMain / 1000 / 60 / 60 / 24 / 31 / 12 ));
+      
+// }
+
+
+// age(`April 21 , 2004`)
+
+
+
+
